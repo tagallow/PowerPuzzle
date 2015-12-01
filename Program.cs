fe60b7815be97b8e7dbd4b4fa2a607e916fa9b71
Tom <tomg555@users.noreply.github.com>
2015-11-30 21:52:47 -0600

    Added correct fair share sequence

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Kts.AStar;
 using System.Diagnostics;
 
 
@@ -15,69 +14,90 @@ namespace PowerPuzzle
         {
             Solve();
         }
-        public static void QueueTest()
-        {
-            RandomMeldablePriorityQueue<int> q = new RandomMeldablePriorityQueue<int>();
-            q.Add(10);
-            q.Add(2);
-            q.Add(5);
-            q.Add(3);
-            q.Add(4);
-            q.Add(12);
 
-            while (q.Count > 0)
-            {
-                Console.WriteLine(q.Dequeue());
-            }
-
-            Console.ReadKey();
-
-        }
         public static void Solve()
         {
-            int _k = 3;
+            int _k = 4;
             int _n = (int)Math.Pow(2, _k + 1);
-            int nodeCount = 0;
-            Stopwatch timer = new Stopwatch();
-            RandomMeldablePriorityQueue<Node> q = new RandomMeldablePriorityQueue<Node>();
-            //Stack<Node> q = new Stack<Node>();
-            List<Node> Solutions = new List<Node>();
-            HashSet<Node> CheckedNodes = new HashSet<Node>();
-            Node FirstNode = new Node() { _k = _k, a = new List<int>(), b = new List<int>(), Cost = 0 };
-            q.Add(FirstNode);
-            timer.Start();
-            while (q.Count > 0)
+
+            List<int> a = new List<int>();
+            List<int> b = new List<int>();
+            string seq = FairShareSequence(_n);
+
+            for (int i = 0; i < _n; i++)
             {
-                Node node = q.Dequeue();
-                nodeCount++;
-                if (node.IsSovled() && !Solutions.Contains(node))
+                if(seq.ToCharArray()[i]=='a')
                 {
-                    //timer.Stop();
-                    Console.WriteLine("===SOLUTION FOUND===");
-                    node.PrintArrays();
-                    Console.WriteLine("{0:0,000} nodes checked", nodeCount);
-                    Console.WriteLine("Time: {0}", timer.Elapsed.ToString());
-                    Solutions.Add(node);
-                    //break;
+                    a.Add(i);
                 }
                 else
                 {
-
-
-                    foreach (Node childNode in node.GetChildren())
-                    {
-                        if (!q.Contains(childNode))
-                        {
-                            //if(!CheckedNodes.Contains(childNode))
-                            q.Add(childNode);
-                        }
-                    }
+                    b.Add(i);
                 }
+            }
+            Console.WriteLine(String.Join(", ", a.ToArray()));
+            Console.WriteLine(String.Join(", ", b.ToArray()));
+            if(IsSolved(a,b,_k))
+            {
+                Console.WriteLine("correct");
+            }
+            else
+            {
+                Console.WriteLine("fail");
             }
 
             Console.WriteLine("Done");
             Console.ReadKey();
 
         }
+
+        private static string FairShareSequence(int n)
+        {
+            string seq = "ab";
+            string seq2 = "";
+            while (seq.Length < n)
+            {
+                foreach (char c in seq.ToCharArray())
+                {
+                    if (c == 'a')
+                    {
+                        seq2 += "ab";
+                    }
+                    else
+                    {
+                        seq2 += "ba";
+                    }
+                }
+                seq = seq2;
+                seq2 = "";
+            }
+
+            return seq;
+
+        }
+        #region solution checks
+        private static bool IsSolved(List<int> a, List<int> b, int _k)
+        {
+            return SumTest(a, b) && SumPowerTest(a, b, _k);
+        }
+        private static bool SumTest(List<int> a, List<int> b)
+        {
+            return a.Sum() == b.Sum();
+        }
+        private static bool SumPowerTest(List<int> a, List<int> b, int _k)
+        {
+            List<int> a2 = new List<int>();
+            List<int> b2 = new List<int>();
+
+            foreach (int n in a)
+                a2.Add((int)Math.Pow(n, _k));
+
+            foreach (int n in b)
+                b2.Add((int)Math.Pow(n, _k));
+
+            return a2.Sum() == b2.Sum();
+        }
+        #endregion
+
     }
 }
